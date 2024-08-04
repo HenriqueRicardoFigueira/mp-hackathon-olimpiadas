b7072777a49aded4a5ebb19fe999d15b29488dba
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-08-04 11:00:50 -0300

    add commands to bot

--- max_atlhetics/app/commands/ping.rb
+++ max_atlhetics/app/commands/ping.rb
@@ -5,10 +5,10 @@ module Ping
   extend Discordrb::Commands::CommandContainer
 
   Bot.command(:ping) do |event|
-    event.respond 'Pong!'
+    event.respond "Pong!"
   end
 
-  Bot.message(content: 'ping') do |event|
-    event.respond 'Pong!'
+  Bot.message(content: "ping") do |event|
+    event.respond "Pong!"
   end
 end
--- max_atlhetics/app/commands/ranking.rb
+++ max_atlhetics/app/commands/ranking.rb
@@ -5,12 +5,12 @@ module Ranking
     event.respond fetch_ranking_medals
   end
 
-  Bot.message(content: 'ranking') do |event|
+  Bot.message(content: "ranking") do |event|
     event.respond fetch_ranking_medals
   end
 
   def self.fetch_ranking_medals
-    brasil = Country.find_by(name: 'Brasil')
+    brasil = Country.find_by(name: "Brasil")
     ranking = Country.order(rank: :asc).limit(10).each.map do |country|
        "#{country.rank} - #{country.name} 🥇 #{country.total_medals} medalhas ![](#{country.flag_url})"
     end
