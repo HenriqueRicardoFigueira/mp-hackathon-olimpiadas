88becddd80cf8dfecfae6bb7cc1672e2277f5de8
HenriqueRicardoFigueira <henrislip@gmail.com>
2024-08-04 11:01:19 -0300

    add bot and good job

--- max_atlhetics/config/initializers/bot.rb
+++ max_atlhetics/config/initializers/bot.rb
@@ -3,8 +3,8 @@
 # chanel_id = 1153312562579001346
 
 Bot = Discordrb::Commands::CommandBot.new(
-  token: ENV['DISCORD_BOT_TOKEN'],
-  prefix: '!'
+  token: ENV["DISCORD_TOKEN"],
+  prefix: "!"
 )
 
 Dir["#{Rails.root}/app/commands/*.rb"].each { |file| require file }
